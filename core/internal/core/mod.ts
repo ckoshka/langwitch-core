3419e97905ab60d2012fc5416038109cde7d4276
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-09-28 11:27:37 +1300

    updated exports

--- core/internal/core/mod.ts
+++ core/internal/core/mod.ts
@@ -1,3 +1,5 @@
 export * from "./memory.ts";
 export type { Memory, MemoryConstantsReader } from "./types/memory.ts";
 export * from "./effects/mod.ts";
+export * from "./types/context.ts";
+export * from "./types/concept.ts";
